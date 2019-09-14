9c562bb1a9fa9c1b488cd41601890e5b723439e3
chennareddysravanicapg <55176504+chennareddysravanicapg@users.noreply.github.com>
2019-09-14 15:55:21 +0530

    change

--- GreatOutdoor/GreatOutdoor.Entities/GreatOutdoor.cs
+++ GreatOutdoor/GreatOutdoor.Entities/GreatOutdoor.cs
@@ -39,52 +39,7 @@ namespace GreatOutdoor.Entities
         }
     }
 
-   public class SalesPerson
-    {
-        
-     }
-    public class Admin
-    {
-        private int adminID;
-
-        public int AdminID
-        {
-            get { return adminID; }
-            set { adminID = value; }
-        }
-        private string adminName;
-
-        public string AdminName
-        {
-            get { return adminName; }
-            set { adminName = value; }
-        }
-        private int adminmobileno;
-
-        public int Adminmobileno
-        {
-            get { return adminmobileno; }
-            set { adminmobileno = value; }
-        }
-        private string adminusername;
-        public string Adminusername
-        {
-            get { return adminusername; }
-            set { adminusername = value; }
-        }
-        private string adminEmail;
-        public string AdminEmail
-        {
-            get { return adminEmail; }
-            set { adminEmail = value; }
-        }
-        private string adminpassword;
-        public string Adminpassword
-        {
-            get { return adminpassword; }
-            set { adminpassword = value; }
-        }
-    }
+  
 
     
  }
