8bc983d9f1ac725d04a459f4662f45936af7af57
Ayush0087 <54570255+Ayush0087@users.noreply.github.com>
2019-09-16 17:06:47 +0530

    .

--- GreatOutdoor/GreatOutdoor.Entities/SalesPerson.cs
+++ GreatOutdoor/GreatOutdoor.Entities/SalesPerson.cs
@@ -6,6 +6,26 @@ namespace GreatOutdoor.Entities
 {
     class SalesPerson
     {
+        private string _salesPersonName;
+        private string _salesPersonemail;
+        private string _salesPersonMobileNumber;
+        private static int _salesPersonID;
+
+        public string SalesPersonName { get => _salesPersonName; set => _salesPersonName = value; }
+        public string SalesPersonemail { get => _salesPersonemail; set => _salesPersonemail = value; }
+        public string SalesPersonMobileNumber { get => _salesPersonMobileNumber; set => _salesPersonMobileNumber = value; }
+        public static int SalesPersonID { get => _salesPersonID; set => _salesPersonID = value; }
+
+        public SalesPerson()
+        {
+
+            _salesPersonName = string.Empty;
+            _salesPersonMobileNumber = string.Empty;
+            _salesPersonemail = string.Empty;
+            _salesPersonID = 0;
+
+
+        }
 
 
 
